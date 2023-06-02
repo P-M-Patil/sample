aa6f07beb2fe901457e246e1325b2af7b882f538
P-M-Patil <135338175+P-M-Patil@users.noreply.github.com>
2023-06-02 18:22:51 +0530

    Add files via upload

---
+++ sample-project/migrations/20230429090029-create-session.js
@@ -0,0 +1,46 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("session", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      sportname: {
+        type: Sequelize.INTEGER,
+      },
+      time: {
+        type: Sequelize.DATE,
+      },
+      address: {
+        type: Sequelize.STRING,
+      },
+      playername: {
+        type: Sequelize.ARRAY(Sequelize.STRING),
+      },
+      noplayers: {
+        type: Sequelize.INTEGER,
+      },
+      sessioncreated: {
+        type: Sequelize.BOOLEAN,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  // eslint-disable-next-line no-unused-vars
+  // © Abhishek Bante
+  // eslint-disable-next-line no-unused-vars
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("session");
+  },
+};
---
+++ sample-project/migrations/20230505053758-create-user.js
@@ -0,0 +1,50 @@
+"use strict";
+
+// eslint-disable-next-line no-unused-vars
+const { sequelize } = require("../models");
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("user", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      fname: {
+        type: Sequelize.STRING,
+      },
+      lname: {
+        type: Sequelize.STRING,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: Sequelize.STRING,
+      },
+      role: {
+        type: Sequelize.STRING,
+      },
+      sessionId: {
+        type: Sequelize.ARRAY(Sequelize.INTEGER),
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  // eslint-disable-next-line no-unused-vars
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("user");
+  },
+};
